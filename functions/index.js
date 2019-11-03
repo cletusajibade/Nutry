c3a8b65d05dc30eb085beb72b49344f5ff6226fd
Cletus Ajibade <cletusajibade@gmail.com>
2019-11-02 19:03:21 -0500

    added getAllUsers endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const {parse} = require('querystring');
 
 admin.initializeApp(functions.config().firebase);
 
 let db = admin.firestore();
 
 /**
- * API endpoint that expects JSON of the form:
+ * API endpoint expects JSON of the form:
  * {
 	"first_name":"value",
 	"last_name":"value",
@@ -18,33 +17,80 @@ let db = admin.firestore();
     }
  * @type {HttpsFunction}
  */
-exports.addUser = functions.https.onRequest(async (req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(401).json({
+
+// Notice the use of "async, await".
+// This helps to handle promises without having to chain them together with the "then" function.
+// But this must be done within "try,catch" block to handle any errors in case the promise was rejected.
+exports.addUser = functions.https.onRequest(async (request, response) => {
+    //Make sure this is a POST request
+    if (request.method !== 'POST') {
+        return response.status(401).json({
             message: 'Not allowed'
         })
     }
 
     try {
-        console.log(req.body);
-        const data = {first_name, last_name, date_of_birth, height, weight, gender} = req.body;
+        console.log(request.body);
+
+        // Destructure the request body into an object with corresponding properties
+        const data = {first_name, last_name, date_of_birth, height, weight, gender} = request.body;
 
         // Add a new document with an auto-generated id.
         // The 'users' collection is created if it does not already exist
         const userRef = await db.collection('users').add(data);
         const user = await userRef.get();
 
-        res.json({
+        response.json({
             id: userRef.id,
             data: user.data()
         });
     } catch (error) {
-        res.status(500).send(error);
+        response.status(500).send(error);
     }
 });
 
+// Using Promise chaining, get all users from the database
+// Notice the absence of "async, await"; promise chaining with "then" is used instead
+exports.getAllUsers = functions.https.onRequest((request, response) => {
+    //Make sure this is a GET request
+    if (request.method !== 'GET') {
+        return response.status(401).json({
+            message: 'Not allowed'
+        })
+    }
+
+    console.log(request.query);
+    if(!request.query.isEmpty){
+        //
+    }
+
+    db.collection('users').get()
+        .then(usersSnapshot => {
+            if (!usersSnapshot.empty) {
+                let docArray = []; //Holds each user in the collection
+                usersSnapshot.forEach(doc => {
+                    const row = {
+                        id: doc.id,
+                        data: doc.data()
+                    };
+                    docArray.push(row);
+                });
+                console.log(docArray);
+
+                //1. "response.send" converts docArray into JSON and sends it to the client
+                //2. It finally terminates this function
+                return response.send(docArray);
+            } else {
+                return response.send({msg: "No users in database"});
+            }
+        })
+        .catch(error => {
+            response.status(500).send(error);
+        })
+});
+
 /**
- * API endpoint that expects JSON of the form:
+ * API endpoint expects JSON of the form:
  * {
 	"food_name":"value",
 	"food_class":"value",
@@ -54,28 +100,32 @@ exports.addUser = functions.https.onRequest(async (req, res) => {
     }
  * @type {HttpsFunction}
  */
-exports.addFood = functions.https.onRequest(async (req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(401).json({
+// Notice the use of "async, await".
+// This helps to handle promises without having to chain them together with the "then" function.
+// But this must be done within "try,catch" block to handle any errors in case the promise was rejected.
+exports.addFood = functions.https.onRequest(async (request, response) => {
+    //Make sure this is a POST request
+    if (request.method !== 'POST') {
+        return response.status(401).json({
             message: 'Not allowed'
         })
     }
 
     try {
-        console.log(req.body);
-        const data = {food_name, food_class, price, serving} = req.body;
+        console.log(request.body);
+        const data = {food_name, food_class, price, serving} = request.body;
 
         // Add a new document with an auto-generated id.
         // The 'food' collection is created if it does not already exist
         const foodRef = await db.collection('food').add(data);
         const food = await foodRef.get();
 
-        res.json({
+        response.json({
             id: foodRef.id,
             data: food.data()
         });
     } catch (error) {
-        res.status(500).send(error);
+        response.status(500).send(error);
     }
 });
 
