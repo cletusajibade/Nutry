314e4b622c0eda53d4e82ed8125164413a2a7ed9
Cletus Ajibade <cletusajibade@gmail.com>
2019-11-29 17:11:46 -0600

    implemented admin auth create user

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const crypto = require('crypto');
-//const authHandler = require('./util/AuthHandler');
+const authHandler = require('./util/AuthHandler');
 
 admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
+let auth = admin.auth();
 
 // if(location.hostname==="localhost"){
 //     db.settings({
@@ -44,16 +44,16 @@ exports.addUser = functions.https.onRequest(async (request, response) => {
         // const data = {first_name, last_name, date_of_birth, height, weight, gender, email, password} = request.body;
         const data = request.body;
 
-        //Hash the password
-        data.password = this.setPassword(data.password);
+        //Get a random salt, and then a hash of the password before saving to firestore
+        const salt = authHandler.getPasswordSalt();
+        data.password = authHandler.getPasswordHash(data.password, salt);
+        data.salt = salt; //this adds a new field on the fly; was not sent along with the request
 
-        console.log(data);
-
-        // authHandler.setPassword(data.password);
         // Add a new document with an auto-generated id.
         // The 'users' collection is created if it does not already exist
         const userRef = await db.collection("users").add(data);
         const user = await userRef.get();
+
         response.json({
             id: userRef.id,
             data: user.data()
@@ -72,8 +72,6 @@ exports.getAllUsers = functions.https.onRequest((request, response) => {
             message: "Not allowed"
         });
     }
-
-    console.log(request.query);
 
     db.collection("users")
         .get()
@@ -124,8 +122,9 @@ exports.addFood = functions.https.onRequest(async (request, response) => {
     }
 
     try {
-        console.log(request.body);
-        const data = ({food_name, food_class, price, serving} = request.body);
+        // Destructure the request body into an object with corresponding properties
+        // const data = {food_name, food_class, price, serving} = request.body;
+        const data = request.body;
 
         // Add a new document with an auto-generated id.
         // The 'food' collection is created if it does not already exist
@@ -141,30 +140,78 @@ exports.addFood = functions.https.onRequest(async (request, response) => {
     }
 });
 
+
 /**
- * Method to set salt and hash the password for a user.
- * setPassword method first creates a salt unique for every user,
- * then it hashes the salt with user password and creates a hash.
- * This hash is stored in the database as user password
+ * API endpoint expects JSON of the form:
+ * {
+	"email":"value",
+    "password":"value",
+    }
+ * @type {HttpsFunction}
  */
-exports.setPassword = function (password) {
-    // Hashing user's salt and password with 1000 iterations, 64 length and sha512 digest
-    const salt = crypto.randomBytes(16).toString('hex');
-    return crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
-};
+exports.login = functions.https.onRequest((request, response) => {
+    //Make sure this is a POST request
+    if (request.method !== "POST") {
+        return response.status(401).json({
+            message: "Not allowed"
+        });
+    }
 
-// exports.getSalt = function () {
-//     // Creating a unique salt for a particular user
-//     return crypto.randomBytes(16).toString('hex');
-// };
+    // db.collection("users")
+    //     .get()
+    //     .then(usersSnapshot => {
+    //         if (!usersSnapshot.empty) {
+    //             let docArray = []; //Holds each user in the collection
+    //             usersSnapshot.forEach(doc => {
+    //                 const row = {
+    //                     id: doc.id,
+    //                     data: doc.data()
+    //                 };
+    //                 docArray.push(row);
+    //             });
+    //
+    //             //1. "response.send" converts docArray into JSON and sends it to the client
+    //             //2. It finally terminates this function
+    //             return response.send(docArray);
+    //         } else {
+    //             return response.send({msg: "No users in database"});
+    //         }
+    //     })
+    //     .catch(error => {
+    //         response.status(500).send(error);
+    //     });
 
-// Method to check the entered password is correct or not.
-// valid password method checks whether the user password is correct or not.
-// It takes the user password from the request and salt from user database entry
-// It then hashes user password and salt,
-// then checks if this generated hash is equal to user's hash in the database or not.
-// If the user's hash is equal to generated hash, then the password is correct otherwise not
-// exports.validatePassword = function (password) {
-//     let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
-//     return this.hash === hash;
-// };
+    const authData = {
+        email: request.body.email,
+        emailVerified: false,
+        password: request.body.password,
+        disabled: false,
+    };
+    const customData = {
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        email: request.body.email,
+        dateOfBirth: request.body.dateOfBirth,
+        height: request.body.height,
+        weight: request.body.weight,
+        gender: request.body.gender,
+        photoURL: request.body.photoURL,
+        avoidFood: request.body.avoidFood,
+        dateCreated: new Date()
+    };
+    auth.createUser(authData)
+        .then((userRecord) => {
+            // On success, create a custom user collection/document
+            // that has the same uid returned by the createUser auth process
+            return db.collection('users').doc(userRecord.uid).set(customData);
+        })
+        .then((writeResult) => {
+            return response.json({
+                message: 'Successfully created new user',
+                time: writeResult.writeTime
+            });
+        })
+        .catch((error) => {
+            response.status(500).send(error);
+        });
+});
